6466a1f18c08f90e5fbd0755037514b17194d710
Mostafa <mostafaamr99533@gmail.com>
2026-03-10 19:30:39 +0200

    + Create login_screen.dart and re_password.dart

--- studex_graduation_project/lib/features/auth/register/register_screen.dart
+++ studex_graduation_project/lib/features/auth/register/register_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:studex_graduation_project/core/theme/app_colors.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+    );
   }
 }
