1607977e1e418700418ef52e6cb4b93a3df65de9
ibraam isaac <ebraamop@gmail.com>
2026-03-13 13:33:44 +0200

    completed start quiz ui

--- studex_graduation_project/lib/features/quiz/screens/start_quiz_screen.dart
+++ studex_graduation_project/lib/features/quiz/screens/start_quiz_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:studex_graduation_project/features/quiz/widgets/abb_bar_start_quiz.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:studex_graduation_project/features/quiz/widgets/start_quiz_widget/abb_bar_start_quiz.dart';
+import 'package:studex_graduation_project/features/quiz/widgets/start_quiz_widget/start_quiz_button.dart';
+import 'package:studex_graduation_project/features/quiz/widgets/start_quiz_widget/start_quiz_details.dart';
+import 'package:studex_graduation_project/features/quiz/widgets/start_quiz_widget/start_quiz_header_image.dart';
+import 'package:studex_graduation_project/features/quiz/widgets/start_quiz_widget/start_quiz_title.dart';
 
 class StartQuizScreen extends StatelessWidget {
   const StartQuizScreen({super.key});
@@ -7,13 +12,31 @@ class StartQuizScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFF8F9FF),
       appBar: StartQuizAppBar(
         title: 'هندسة',
         subtitle: 'الترم الاول',
         onMenuPressed: () {},
         onArrowPressed: () {},
       ),
-      body: SizedBox.shrink(),
+      body:
+    Column(
+      children: [
+        StartQuizHeaderImage(
+              imageUrl:
+                  'https://media.istockphoto.com/id/1616906708/vector/vector-speech-bubble-with-quiz-time-words-trendy-text-balloon-with-geometric-grapic-shape.jpg?s=2048x2048&w=is&k=20&c=5Yp0ha5yM9YQGZx01kR3bGh-M9H4N2XEJLGDOXe7R1o=',
+            ),
+        StartQuizTitleSection(
+          title: 'اختبار الوحدة الأولى - أساسيات',
+          subtitle: 'يتضمن هذا الاختبار مفاهيم هندسة البرمجيات الأساسية، دورة حياة النظام، والنماذج المختلفة للتطوير. استعد جيداً!',
+        ),
+        SizedBox(height: 16.h),
+        StartQuizDetailsSection(participantsCount: '12', timeValue: '20', questionsCount: '15',),
+        SizedBox(height: 40.h),
+        StartQuizButton(label: 'بدء الجلسة الآن', onPressed: () {  },),
+      ],
+    ),
+
     );
   }
 }
