fde00048c1f5d0caffcdc7e3a8886080cda3aeee
ibraam isaac <ebraamop@gmail.com>
2026-03-13 08:37:24 +0200

    edit main function same develop

--- studex_graduation_project/lib/main.dart
+++ studex_graduation_project/lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_localizations/flutter_localizations.dart'; // تأكد من استيراد هذه المكتبة
 import 'package:studex_graduation_project/routes/app_router_generation.dart';
 
 import 'core/theme/app_themes.dart';
-
-
 
 void main() {
   runApp(const MyApp());
@@ -13,22 +12,33 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
-
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: Size(390, 884),
+      designSize: const Size(390, 884),
+      minTextAdapt: true,
+      splitScreenMode: true,
       builder: (context, child) {
         return MaterialApp.router(
           debugShowCheckedModeBanner: false,
-          title: 'Finance App ',
+          title: 'Studex App',
           theme: AppThemes.lightTheme,
+
+          locale: const Locale('ar', 'EG'),
+          supportedLocales: const [
+            Locale('ar', 'EG'),
+            Locale('en', 'US'),
+          ],
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          // --------------------------------------
+
           routerConfig: RouterGenerationConfig.goRouter,
         );
       },
     );
   }
 }
-
-
